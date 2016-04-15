352befc9faac00765822ede3bf396ad78a64d360
a00909032 <a00909032@gmail.com>
2016-04-15 07:04:50 -0700

    1. admin page done

--- application/views/_gameStatus.php
+++ application/views/_gameStatus.php
@@ -5,18 +5,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 ?>
 <div style="text-align: center">
-<h4>Round {round}</h4>
-<p>Game Status:<b style="text-transform: uppercase">{current}</b></p>
-<?php
- if ($_SESSION['register'] == "open"){
-     echo '<a href="home/agentregister">Click here to register</a>';
- } else 
-     if ($_SESSION['register'] == "okay"){
-     echo '<a href="home/agentregister">Click here to buy cards</a>';
-     echo '<a href="home/agentregister">Click here to sell cards</a>';
-     }
- else {
-     echo 'Registration is closed';    
-}
-?>
+    <h4>Round {round}</h4>
+    <p>Game Status:<b style="text-transform: uppercase">{current}</b></p>
+    <?php
+    if ($_SESSION['register'] == "open") {
+        echo '<a href="home/agentregister">Click here to register</a>';
+    } else
+    if ($_SESSION['register'] == "okay") {
+        echo '<a href="home/agentregister">Click here to buy cards</a>';
+        echo '<a href="home/agentregister">Click here to sell cards</a>';
+    } else {
+        echo 'Registration is closed';
+    }
+    ?>
 </div>
